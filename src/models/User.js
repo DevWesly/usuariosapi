08ccbc47c2e659e6a870deaccfaec18d83b9ecb3
DevWesly <dev.lucaswesley@gmail.com>
2023-07-14 14:15:31 -0300

    Rotas, Conexão  e relacionamento entre Usuarios e seus endereços

--- src/models/User.js
+++ src/models/User.js
@@ -1,15 +1,20 @@
-const {Model, DataTypes} = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
 class User extends Model {
-    static init (sequelize){
-        super.init({
-            name: DataTypes.STRING,
-            email: DataTypes.STRING,
-        },{
-            sequelize
-        })
-
-    }
+  static init(sequelize) {
+    super.init(
+      {
+        name: DataTypes.STRING,
+        email: DataTypes.STRING,
+      },
+      {
+        sequelize,
+      }
+    );
+  }
+  static associate(models){
+    this.hasMany(models.Address, {foreignKey:'user_id', as:'addresses'})
+  }
 }
 
 module.exports = User;
--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,28 @@
 const express = require ('express');
 const UserController = require('./controllers/userController');
+const AddressController = require('./controllers/addressController');
 
 const routes = express.Router();
 
+//Rotas de Usuarios
 routes.get('/index',UserController.index);
 routes.get('/index/:id',UserController.user);
-
 routes.post('/users',UserController.store);
+routes.put('/att/:id',UserController.att);
+routes.delete('/del/:id',UserController.del);
+
+//Rotas de Enderessos
+routes.get('/users/addresses', AddressController.allAddresses);
+routes.get('/user/:user_id/address', AddressController.endereco);
 
 
-routes.put('/att/:id',UserController.att)
+routes.post('/user/:user_id/address', AddressController.storeAddress);
 
-routes.delete('/del/:id',UserController.del)
+
+
+
+
+
 
 
 module.exports = routes;
