c8078cb06004dcd3b4cd62bdc58293cb6b0d87fa
DevWesly <dev.lucaswesley@gmail.com>
2023-07-17 09:36:24 -0300

    rotas de Jobs e configurando para ser reconhecido pelo servidor

--- src/database/index.js
+++ src/database/index.js
@@ -3,15 +3,19 @@ const Sequelize = require("sequelize");
 const dbConfig = require("../config/database");
 
 const User = require("../models/User");
-
 const Address = require("../models/Address");
+const Job = require("../models/Job");
+
 
 const connection = new Sequelize(dbConfig);
 
+
 User.init(connection);
 Address.init(connection);
+Job.init(connection);
 
 User.associate(connection.models);
 Address.associate(connection.models);
+Job.associate(connection.models);
 
 module.exports = connection;
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 const express = require ('express');
 const UserController = require('./controllers/userController');
 const AddressController = require('./controllers/addressController');
+const JobController = require('./controllers/jobController');
 
 const routes = express.Router();
 
@@ -14,9 +15,16 @@ routes.delete('/del/:id',UserController.del);
 //Rotas de Enderessos
 routes.get('/users/addresses', AddressController.allAddresses);
 routes.get('/user/:user_id/address', AddressController.endereco);
-
-
 routes.post('/user/:user_id/address', AddressController.storeAddress);
+
+
+//Rotas de Trabalho
+
+routes.get('/users/jobs', JobController.index);
+routes.post('/user/:user_id/jobs', JobController.create);
+
+
+
 
 
 
